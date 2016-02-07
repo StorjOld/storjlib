bc0ef0629a09a8b4993b40771cead52becd519c8
F483 <fabian.barkhau@gmail.com>
2016-02-06 19:58:05 -0500

    validate input for audit proof validation

--- storjlib/api.py
+++ storjlib/api.py
@@ -1,3 +1,4 @@
+import six
 import apigen
 import storjlib
 from . version import __version__  # NOQA
@@ -20,7 +21,16 @@ class Storjlib(apigen.Definition):
 
     @apigen.command()
     def audit_validate(self, proof, root, challengenum, leaves):
-        # TODO validate input
+
+        # validate input
+        assert(isinstance(proof, list))
+        assert(storjlib.util.is_hex_hash(root))
+        assert(isinstance(leaves, list))
+        for leaf in leaves:
+            assert(storjlib.util.is_hex_hash(leaf))
+        assert(isinstance(challengenum, six.integer_types))
+        assert(0 <= challengenum < len(leaves))
+
         return storjlib.audit.validate(proof, root, challengenum, leaves)
 
 
